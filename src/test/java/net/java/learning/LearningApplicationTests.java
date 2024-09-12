9acb46377a9fdd175cbf962023c177ad0cd8823d
unknown <jeetshahajwa@gmail.com>
2024-09-12 23:32:41 +0530

    New Spring App

---
+++ src/test/java/net/java/learning/LearningApplicationTests.java
@@ -0,0 +1,13 @@
+package net.java.learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
